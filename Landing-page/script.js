a48ae4f97f13b9717d1feb8343ea126feec34bab
Mariana Gutierrez Almaguer <marianagutierreza19@gmail.com>
2026-03-23 13:44:35 -0600

    Contenedor trending con contenedor de cards

--- Landing-page/script.js
+++ Landing-page/script.js
@@ -10,5 +10,23 @@ textos.forEach(function(elemento) {
 
 // Contenedor TRENDING
 const contenedor = document.querySelector(".trending")
-const texto1 = document.querySelectorAll("h4")
-texto1[0].innerText = "CONJUNTOS"
+
+const joggers1 = document.querySelectorAll("h4")
+joggers1[0].innerText = "JOGGERS"
+const joggers2 = document.querySelectorAll("p")
+joggers2[0].innerText = "Un conjunto cómodo y transpirable que mantiene tu enfoque donde más lo necesitas."
+
+const seamless1 = document.querySelectorAll("h4")
+seamless1[1].innerText = "SEAMLESS"
+const seamless2 = document.querySelectorAll("p")
+seamless2[1].innerText = "Todos los aman, y tú también lo harás."
+
+const sudaderas1 = document.querySelectorAll("h4")
+sudaderas1[2].innerText = "SUDADESAS"
+const sudaderas2 = document.querySelectorAll("p")
+sudaderas2[2].innerText = "La prenda de transición definitiva que nunca falla."
+
+const tops1 = document.querySelectorAll("h4")
+tops1[3].innerText = "TOPS"
+const tops2 = document.querySelectorAll("p")
+tops2[3].innerText = "Elige un sostén deportivo que se mueva contigo."
