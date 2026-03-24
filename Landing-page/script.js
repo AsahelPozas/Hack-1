a311d0ebfd2810e33536ba6eb3bbfe36c7561672
LuiCaba <LuiCaba@outlook.es>
2026-03-23 19:32:26 -0600

    se corrigio el banner

--- Landing-page/script.js
+++ Landing-page/script.js
@@ -14,23 +14,23 @@ const contenedor = document.querySelector(".trending");
 const joggers1 = document.querySelectorAll("h4");
 joggers1[0].innerText = "JOGGERS";
 const joggers2 = document.querySelectorAll("p");
-joggers2[0].innerText =
+joggers2[2].innerText =
     "Un conjunto cómodo y transpirable que mantiene tu enfoque donde más lo necesitas.";
 
 const seamless1 = document.querySelectorAll("h4");
 seamless1[1].innerText = "SEAMLESS";
 const seamless2 = document.querySelectorAll("p");
-seamless2[1].innerText = "Todos los aman, y tú también lo harás.";
+seamless2[3].innerText = "Todos los aman, y tú también lo harás.";
 
 const sudaderas1 = document.querySelectorAll("h4");
 sudaderas1[2].innerText = "PLAYERAS";
 const sudaderas2 = document.querySelectorAll("p");
-sudaderas2[2].innerText = "La prenda de transición definitiva que nunca falla.";
+sudaderas2[4].innerText = "La prenda de transición definitiva que nunca falla.";
 
 const tops1 = document.querySelectorAll("h4");
 tops1[3].innerText = "TOPS";
 const tops2 = document.querySelectorAll("p");
-tops2[3].innerText = "Elige un sostén deportivo que se mueva contigo.";
+tops2[5].innerText = "Elige un sostén deportivo que se mueva contigo.";
 function addToCart(element) {
     let carro;
     if (!localStorage.getItem("cart")) {
